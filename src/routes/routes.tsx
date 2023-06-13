9b86009e823590767f4b188c9de73bfae9a9c61a
CrazyBucket <1475743203@qq.com>
2023-06-13 14:49:36 +0800

    🐛 修正路由问题

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -4,9 +4,6 @@ import Resource from '@/pages/centerOverview/resource/resource'
 import Rules from '@/pages/centerOverview/rules/rules'
 import ContactUs from '@/pages/contactUs'
 import Install from '@/pages/contactUs/install/install'
-import Manage from '@/pages/contactUs/manage/manage'
-import VPN from '@/pages/contactUs/VPN/VPN'
-import Home from '@/pages/home'
 import News from '@/pages/news'
 import Cooperate from '@/pages/news/cooperate/cooperate'
 import Release from '@/pages/news/release/release'
@@ -15,7 +12,9 @@ import ApplyAccount from '@/pages/onBoard/applyAccount/applyAccount'
 import Doc from '@/pages/onBoard/doc/doc'
 import Problem from '@/pages/onBoard/problem/problem'
 import UserManual from '@/pages/onBoard/userManual/userManual'
+import Home from '@/pages/home'
 import { Navigate } from 'react-router-dom'
+import NotFound from '@/pages/notFound'
 
 //创建路由表
 const routes = [
@@ -26,78 +25,66 @@ const routes = [
   {
     path: '/centerOverview',
     element: <CenterOverview />,
-    children: [
-      {
-        path: 'introduction',
-        element: <Introduction />,
-      },
-      {
-        path: 'rules',
-        element: <Rules />,
-      },
-      {
-        path: 'resource',
-        element: <Resource />,
-      },
-    ],
+  },
+  {
+    path: '/centerOverview/introduction',
+    element: <Introduction />,
+  },
+  {
+    path: '/centerOverview/rules',
+    element: <Rules />,
+  },
+  {
+    path: '/centerOverview/resource',
+    element: <Resource />,
   },
   {
     path: '/contactUs',
     element: <ContactUs />,
-    children: [
-      {
-        path: 'install',
-        element: <Install />,
-      },
-      {
-        path: 'manage',
-        element: <Manage />,
-      },
-      {
-        path: 'VPN',
-        element: <VPN />,
-      },
-    ],
+  },
+  {
+    path: '/contactUs/install',
+    element: <Install />,
   },
   {
     path: '/news',
     element: <News />,
-    children: [
-      {
-        path: 'cooperate',
-        element: <Cooperate />,
-      },
-      {
-        path: 'release',
-        element: <Release />,
-      },
-    ],
+  },
+  {
+    path: '/news/cooperate',
+    element: <Cooperate />,
+  },
+  {
+    path: '/news/release',
+    element: <Release />,
   },
   {
     path: '/onBoard',
     element: <OnBoard />,
-    children: [
-      {
-        path: 'applyAccount',
-        element: <ApplyAccount />,
-      },
-      {
-        path: 'doc',
-        element: <Doc />,
-      },
-      {
-        path: 'problem',
-        element: <Problem />,
-      },
-      {
-        path: 'userManual',
-        element: <UserManual />,
-      },
-    ],
+  },
+  {
+    path: '/onBoard/applyAccount',
+    element: <ApplyAccount />,
+  },
+  {
+    path: '/onBoard/doc',
+    element: <Doc />,
+  },
+  {
+    path: '/onBoard/problem',
+    element: <Problem />,
+  },
+  {
+    path: '/onBoard/userManual',
+    element: <UserManual />,
   },
   {
     path: '/',
     element: <Navigate to="/home" />,
+  },
+  {
+    path: '*',
+    element: <NotFound />,
   },
 ]
 
