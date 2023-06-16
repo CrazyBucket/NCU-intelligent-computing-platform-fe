8f6460d66e627b22aec0eb4b92c92c229f74cf85
CrazyBucket <1475743203@qq.com>
2023-06-16 22:08:45 +0800

    ✨ 更新导航栏

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -7,20 +7,28 @@ import './index.css'
 const Nav: FC = () => {
   const navigate = useNavigate()
   const [hoverIndex, setHoverIndex] = React.useState<number | null>(null)
-  const tagRoute = ['/', '/centerOverview', '/onBoard', '/news', '/contactUs']
+  const tagRoute = ['/', '/Overview', '/onBoard', '/news', '/contactUs', '/business']
   const subTagRoute = [
     [],
-    ['/centerOverview/introduction', '/centerOverview/resource', '/centerOverview/rules'],
-    ['/onBoard/applyAccount', '/onBoard/doc', '/onBoard/userManual', '/onBoard/problem'],
-    ['/news/release', '/news/cooperate'],
+    ['/Overview/introduction', '/Overview/resource', '/Overview/rules'],
+    [
+      '/onBoard/applyAccount',
+      '/onBoard/doc',
+      '/onBoard/userManual',
+      '/onBoard/problem',
+      './onBoard/application',
+    ],
+    [],
     ['/contactUs/VPN', '/contactUs/install', '/contactUs/manage'],
+    ['./business/onCampus', './business/enterprise', './business/consult'],
   ]
   const subTags = [
     [],
-    ['智算平台简介', '中心资源', '规章制度'],
-    ['账号申请', '文档与培训', '用户手册', '常见问题'],
-    ['新闻发布', '科研项目合作'],
-    ['校外用户申请开通超算中心VPN', '软件安装协助', '智算中心管理'],
+    ['智算平台简介', '平台资源', '规章制度'],
+    ['账号申请', '文档与培训', '用户手册', '常见问题', '付费申请'],
+    [],
+    ['校外用户申请开通超算中心VPN', '软件安装协助', '智算平台管理'],
+    ['校内科研合作', '校企科研合作', '项目咨询与需求发布'],
   ]
 
   return (
@@ -43,61 +51,63 @@ const Nav: FC = () => {
         }}
       >
         <div style={{ display: 'flex', alignItems: 'center' }}>
-          {['主页', '中心概况', '上机服务', '新闻公告', '联系我们'].map((tag, index) => (
-            <motion.div
-              key={index}
-              onHoverStart={() => setHoverIndex(index)}
-              onHoverEnd={() => setHoverIndex(null)}
-              style={{ position: 'relative' }}
-            >
-              <div
-                className="navTag"
-                onClick={() => {
-                  navigate(tagRoute[index])
-                }}
+          {['主页', '平台概况', '上机服务', '新闻公告', '联系我们', '平台业务'].map(
+            (tag, index) => (
+              <motion.div
+                key={index}
+                onHoverStart={() => setHoverIndex(index)}
+                onHoverEnd={() => setHoverIndex(null)}
+                style={{ position: 'relative' }}
               >
-                {tag}
-              </div>
-              <AnimatePresence initial={false}>
-                {hoverIndex === index && (
-                  <motion.div
-                    className="subTagContainer"
-                    initial="collapsed"
-                    animate="open"
-                    exit="collapsed"
-                    variants={{
-                      open: { opacity: 1, height: 'auto' },
-                      collapsed: { opacity: 0, height: 0 },
-                    }}
-                    transition={{ duration: 0.3, ease: [0.04, 0.62, 0.23, 0.98] }}
-                    style={{
-                      position: 'absolute',
-                      top: '60px',
-                      left: 0,
-                      zIndex: 1,
-                    }}
-                  >
-                    {subTags[index].map((subTag, i) => (
-                      <motion.div
-                        key={subTag}
-                        className="subTag"
-                        variants={{
-                          collapsed: { scale: 0.8, opacity: 0 },
-                          open: { scale: 1, opacity: 1 },
-                        }}
-                        transition={{ duration: 0.3 }}
-                        onClick={() => {
-                          navigate(subTagRoute[index][i])
-                        }}
-                      >
-                        {subTag}
-                      </motion.div>
-                    ))}
-                  </motion.div>
-                )}
-              </AnimatePresence>
-            </motion.div>
-          ))}
+                <div
+                  className="navTag"
+                  onClick={() => {
+                    navigate(tagRoute[index])
+                  }}
+                >
+                  {tag}
+                </div>
+                <AnimatePresence initial={false}>
+                  {hoverIndex === index && (
+                    <motion.div
+                      className="subTagContainer"
+                      initial="collapsed"
+                      animate="open"
+                      exit="collapsed"
+                      variants={{
+                        open: { opacity: 1, height: 'auto' },
+                        collapsed: { opacity: 0, height: 0 },
+                      }}
+                      transition={{ duration: 0.3, ease: [0.04, 0.62, 0.23, 0.98] }}
+                      style={{
+                        position: 'absolute',
+                        top: '60px',
+                        left: 0,
+                        zIndex: 1,
+                      }}
+                    >
+                      {subTags[index].map((subTag, i) => (
+                        <motion.div
+                          key={subTag}
+                          className="subTag"
+                          variants={{
+                            collapsed: { scale: 0.8, opacity: 0 },
+                            open: { scale: 1, opacity: 1 },
+                          }}
+                          transition={{ duration: 0.3 }}
+                          onClick={() => {
+                            navigate(subTagRoute[index][i])
+                          }}
+                        >
+                          {subTag}
+                        </motion.div>
+                      ))}
+                    </motion.div>
+                  )}
+                </AnimatePresence>
+              </motion.div>
+            ),
+          )}
         </div>
         <div style={{ display: 'flex', alignItems: 'center' }}></div>
       </div>
