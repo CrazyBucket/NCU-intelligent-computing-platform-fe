b701e02021490155b33a38d36f6627f92d82f198
CrazyBucket <1475743203@qq.com>
2023-06-16 22:08:22 +0800

    🔥 移除废弃文件

--- src/pages/news/cooperate/cooperate.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from 'react'
-import './index.css'
-
-const Cooperate: FC = () => {
-  return <div>Cooperate</div>
-}
-
-export default Cooperate
--- src/pages/news/release/release.tsx
+++
@@ -1,8 +0,0 @@
-import { FC } from 'react'
-import './index.css'
-
-const Release: FC = () => {
-  return <div>Release</div>
-}
-
-export default Release
