03fd30655d3727bb44d763fc02b3001ad674c6e3
CrazyBucket <1475743203@qq.com>
2023-06-17 23:58:27 +0800

    ✨ 新增404页面

--- src/pages/notFound/index.tsx
+++ src/pages/notFound/index.tsx
@@ -1,7 +1,19 @@
 import { FC } from 'react'
+import notFound from '@/assets/404.svg'
 
 const NotFound: FC = () => {
-  return <div>404 page</div>
+  return (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: '80px',
+      }}
+    >
+      <img src={notFound} alt="" />
+    </div>
+  )
 }
 
 export default NotFound
