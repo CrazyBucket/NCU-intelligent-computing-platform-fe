67d27f890599cd747a1d5e610081b5db6224a07e
CrazyBucket <1475743203@qq.com>
2023-07-11 21:03:21 +0800

    🎨 更新各页面内容

--- src/pages/Serve/index.tsx
+++ src/pages/Serve/index.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react'
 import book from '@/assets/book.svg'
-import doc from '@/assets/doc.svg'
+import release from '@/assets/release.svg'
 import QA from '@/assets/QA.svg'
 import person from '@/assets/person.svg'
 import Card from '@/components/card/card'
@@ -15,7 +15,6 @@ const Serve: FC = () => {
         subTitle="点击进入申请账号详情页"
         navigateTo="/Serve/applyAccount"
       />
-      <Card icon={doc} title="文档与培训" subTitle="查看详细文档" navigateTo="/Serve/doc" />
       <Card
         icon={book}
         title="用户手册"
@@ -23,6 +22,12 @@ const Serve: FC = () => {
         navigateTo="/Serve/userManual"
       />
       <Card icon={QA} title="常见问题" subTitle="查看常见问题及解答" navigateTo="/Serve/problem" />
+      <Card
+        icon={release}
+        title="付费申请"
+        subTitle="查看申请需求"
+        navigateTo="/Serve/application"
+      />
     </div>
   )
 }
--- src/pages/overview/index.tsx
+++ src/pages/overview/index.tsx
@@ -3,6 +3,7 @@ import Card from '@/components/card/card'
 import intro from '@/assets/intro.svg'
 import contact from '@/assets/contact.svg'
 import rule from '@/assets/rule.svg'
+import resource from '@/assets/resource.svg'
 import './index.css'
 
 const Overview: FC = () => {
@@ -13,6 +14,12 @@ const Overview: FC = () => {
         title="智算平台简介"
         subTitle="南昌大学智算平台，为用户提供超算服务"
         navigateTo="/Overview/introduction"
+      />
+      <Card
+        icon={resource}
+        title="平台资源"
+        subTitle="智算平台拥有的资源配置"
+        navigateTo="/Overview/resource"
       />
       <Card
         icon={rule}
