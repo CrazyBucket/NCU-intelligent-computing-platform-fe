994edbc7e6b6ce9a3a0b02967eb131a78ec9badb
CrazyBucket <1475743203@qq.com>
2023-07-12 16:21:22 +0800

    ✨ 解决部分未开放页面

--- src/pages/business/consult/consult.tsx
+++ src/pages/business/consult/consult.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react'
 import './index.css'
 
 const Consult: FC = () => {
-  return <div>consult</div>
+  return <div className="notOpen">该功能暂未开放，敬请期待！</div>
 }
 
 export default Consult
--- src/pages/business/enterprise/enterprise.tsx
+++ src/pages/business/enterprise/enterprise.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react'
 import './index.css'
 
 const Enterprise: FC = () => {
-  return <div>Enterprise</div>
+  return <div className="notOpen">该功能暂未开放，敬请期待！</div>
 }
 
 export default Enterprise
