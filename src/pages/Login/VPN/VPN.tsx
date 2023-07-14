44f2dc1d6232ff57aca077cc98821a4faad7d01a
CrazyBucket <1475743203@qq.com>
2023-07-14 20:43:33 +0800

    ✨ VPN登陆界面

--- src/pages/Login/VPN/VPN.tsx
+++ src/pages/Login/VPN/VPN.tsx
@@ -2,7 +2,31 @@ import { FC } from 'react'
 import './index.css'
 
 const VPN: FC = () => {
-  return <div>VPN</div>
+  return (
+    <div style={{ marginTop: '30px' }}>
+      <div className="textBox">
+        <p style={{ fontSize: '24px', paddingTop: '30px' }}>步骤：</p>
+        <div style={{ paddingLeft: '20px', marginBottom: '30px' }}>
+          <p style={{ fontSize: '20px' }}>先登陆：</p>
+          <a
+            href="http://222-204-28-249-7090-p.dxvpn.ncu.edu.cn:8118"
+            style={{ paddingLeft: '20px' }}
+            target="_blank"
+          >
+            http://222-204-28-249-7090-p.dxvpn.ncu.edu.cn:8118
+          </a>
+          <p style={{ fontSize: '20px', paddingTop: '30px' }}>登陆完成出现网页之后点击：</p>
+          <a
+            href="http://222-204-28-249-7090-p.dxvpn.ncu.edu.cn:8118/login/loginPage.action8"
+            style={{ paddingLeft: '20px' }}
+            target="_blank"
+          >
+            http://222-204-28-249-7090-p.dxvpn.ncu.edu.cn:8118/login/loginPage.action8
+          </a>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default VPN
