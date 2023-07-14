71f264efc05ccbf6cac12d432f525a29da286275
CrazyBucket <1475743203@qq.com>
2023-07-14 20:43:17 +0800

    ✨ 软件安装协助界面

--- src/pages/Login/help/install.tsx
+++ src/pages/Login/help/install.tsx
@@ -1,8 +1,16 @@
+import DownloadBtn from '@/components/downloadBtn/downloadBtn'
 import { FC } from 'react'
+import zip1 from '@/files/Xshell7.zip'
+import zip2 from '@/files/Xftp7.zip'
 import './index.css'
 
 const Help: FC = () => {
-  return <div>Help</div>
+  return (
+    <div className="cardGroup">
+      <DownloadBtn title="软件协助1" subTitle="Xshell7.zip" fileRoute={zip1} />
+      <DownloadBtn title="软件协助2" subTitle="Xftp7.zip" fileRoute={zip2} />
+    </div>
+  )
 }
 
 export default Help
