04e698ac504df0ddd1932e8acbb899b6931cf8a3
CrazyBucket <1475743203@qq.com>
2023-07-14 20:43:59 +0800

    ✨ 联系我们界面

--- src/pages/overview/ContactUs/index.tsx
+++ src/pages/overview/ContactUs/index.tsx
@@ -1,8 +1,36 @@
 import { FC } from 'react'
+import useClipboard from 'react-use-clipboard'
+import toast from 'react-hot-toast'
+import MoudleTitle from '@/components/moudleTitle/moudleTitle'
 import './index.css'
 
 const ContactUs: FC = () => {
-  return <div>ContactUs</div>
+  const [isCopied, setCopied] = useClipboard('ncushuguang@qq.com')
+  const [copy, setCopy] = useClipboard('748925603')
+  const handleToast = () => {
+    toast.success('复制成功')
+  }
+  return (
+    <div>
+      <MoudleTitle title="联系我们" icon="" />
+      <div
+        className="textBox"
+        onClick={() => {
+          setCopied(), handleToast()
+        }}
+      >
+        邮箱：ncushuguang@qq.com
+      </div>
+      <div
+        className="textBox"
+        onClick={() => {
+          setCopy(), handleToast()
+        }}
+      >
+        Q Q : 748925603
+      </div>
+    </div>
+  )
 }
 
 export default ContactUs
