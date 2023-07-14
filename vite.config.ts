f08d74b688d4d76d59f70e0767607cad52600638
CrazyBucket <1475743203@qq.com>
2023-07-14 20:39:54 +0800

    🔧 修改配置文件

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  assetsInclude: ['**/*.docx', '**/*.xlsx'],
+  assetsInclude: ['**/*.docx', '**/*.xlsx', '**/*.zip'],
   plugins: [
     react(),
     vitePluginImp({
